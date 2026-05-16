6fbe8fe8cf520c417102e1157d9f73e93eb2ca70
Diego Souza <dyegosylva95@gmail.com>
2026-05-16 01:52:18 -0300

    fix: use client directive

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import BrotinhoApp from '../brotinho-final'
+"use client"
+import dynamic from 'next/dynamic'
+
+const BrotinhoApp = dynamic(() => import('../brotinho-final'), { ssr: false })
 
 export default function Home() {
   return <BrotinhoApp />
